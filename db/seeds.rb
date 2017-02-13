36f334bd0eb04614b4dabb2a72b1e769a627cbf5
Michael <mcittar@gmail.com>
2017-02-13 14:43:59 -0800

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+kenny = User.create!(name: 'Kenny', email: 'kenny@gmail.com')
+cartman = User.create!(name: 'Eric Cartman', email: 'ecartman@gmail.com')
+chef = User.create!(name: 'Chef', email: 'chef@gmail.com')
