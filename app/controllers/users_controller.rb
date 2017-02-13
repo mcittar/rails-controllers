ada3db32d6145fb1043be10d08c1da07202e738a
Michael <mcittar@gmail.com>
2017-02-13 14:33:49 -0800

    Create user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+    render text: "I'm in the index action!"
+  end
+
+  def show
+    render text: "I'm showin' it!"
+  end
+
+  def create
+    render text: "I'm creating it!"
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+end
