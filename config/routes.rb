b49804089d4d6d8011b3227df8c39333f7574056
Michael <mcittar@gmail.com>
2017-02-13 17:34:58 -0800

    Refactor to 'resources'

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  # resources :users
+  resources :users, only: [:create, :destroy, :index, :show, :update]
 
-  get 'users/:id' => 'users#show', :as => 'user'
-  get 'users' => 'users#index'
-  get 'users/new' => 'users#new', :as => 'new_user'
-  get 'users/:id/edit' => 'users#edit', :as => 'edit_user'
-  post 'users' => 'users#create'
-  patch 'users/:id' => 'users#update'
-  put 'users/:id' => 'users#update'
-  delete 'users/:id/destroy' =>  'users#destroy'
+  # get 'users/:id' => 'users#show', :as => 'user'
+  # get 'users' => 'users#index'
+  # get 'users/new' => 'users#new', :as => 'new_user'
+  # get 'users/:id/edit' => 'users#edit', :as => 'edit_user'
+  # post 'users' => 'users#create'
+  # patch 'users/:id' => 'users#update'
+  # put 'users/:id' => 'users#update'
+  # delete 'users/:id' =>  'users#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
